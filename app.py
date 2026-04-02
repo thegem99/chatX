6d11e91650c5f28ae1cd72e7bcd8173095c82e50
thegem99 <cruxzux@gmail.com>
2026-04-02 17:01:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+# Make sure this is exactly a string pointing to the templates folder
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
+    # Only pass the request inside a dict
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/signup", response_class=HTMLResponse)
